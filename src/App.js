1e882a5672cec800d44c0d963dcfd096538bb5c2
Kumaresh.M <kk8180183@gmail.com>
2022-05-31 19:18:27 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+// credit : https://cssgradient.io
+//credit: https://superdevresources.com/glassmorphism-ui-css/
+// credit: https://hype4.academy/tools/glassmorphism-generator
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state={
+        number:0
+    };
+
 }
+
+add=()=>{
+     this.setState({number:this.state.number+1});
+}
+sub=()=>{
+    this.setState({number:this.state.number-1});
+}
+
+
+  render() {
+    return (
+      <div className="parent">
+      <div className="childglass">
+        <div className="text"><h1>COUNTER APP</h1></div>
+        <div className="h">
+        <div className="num">{this.state.number}</div></div>
+        <div className="asbtn">
+          <button onClick={this.add} className="add">ADD1</button>
+          <button onClick={this.sub} className="sub">SUB1</button>
+        </div>
+      </div>
+    </div>
+    );
+  }
+}
+
+
 
 export default App;
